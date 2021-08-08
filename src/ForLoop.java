42000838dfc255cf1bcc613a9f39ee25d135c924
Lukasz Kaszczyszyn <lukasz.kaszczyszyn@gmail.com>
2021-08-08 21:46:05 +0200

    add exercises

--- src/ForLoop.java
+++ src/ForLoop.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class ForLoop {
     public static void main(String[] args) {
         int i = 0;
+        System.out.println("Ile gwiazdek Ci wydrukowac: ");
+        int starMax = new Scanner(System.in).nextInt();
+
         for (i = 0; i < 5; i++) {
             System.out.println("krok: " + i);
         }
