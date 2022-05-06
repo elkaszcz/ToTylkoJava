3ee05453d9854cfed8dc3f6376536d61704c7a7b
Lukasz Kaszczyszyn <lukasz.kaszczyszyn@gmail.com>
2022-05-06 06:43:33 +0200

    fix unused variable issue

--- src/PrimitiveTypes.java
+++ src/PrimitiveTypes.java
@@ -7,8 +7,8 @@ public class PrimitiveTypes {
 //        x = 8;
 //        System.out.println("x = " + x);
 //
-//        x = x / y;
-//        System.out.println("x = " + x);
+       x = x / y;
+       System.out.println("x = " + x);
 
         x = 2147483647;
         System.out.println(x);
